e0881fc535decac309c66fe6cb9fef0ab4927e87
Enes Buyuk <contact@enesbuyuk.com>
2025-05-09 02:01:39 +0300

    feat: add a function to check supported Vulkan extensions and layers

--- src/instance.h
+++ src/instance.h
@@ -2,6 +2,68 @@
 #include "config.h"
 
 namespace vkInit{
+    bool supported(std::vector<const char*>& extensions, std::vector<const char*>& layers, bool debug){
+        std::vector<vk::ExtensionProperties> supportedExtensions = vk::enumerateInstanceExtensionProperties();
+
+        if (debug){
+            std::cout << "device can support the following extensions: " << std::endl;
+            for (vk::ExtensionProperties ext : supportedExtensions){
+                std::cout << "\t\"" << ext.extensionName << "\"\n";
+            }
+        }
+
+        bool extensionFound = false;
+        std::vector<const char*> notFoundExtensions;
+
+        for (const char* extensionName : extensions){
+            for (vk::ExtensionProperties supportedExtension : supportedExtensions ){
+                if (strcmp(extensionName, supportedExtension.extensionName) == 0 ){
+                    notFoundExtensions.push_back(extensionName);
+                    extensionFound = true;
+                }
+            }
+        }
+
+        // check layer support
+        std::vector<vk::LayerProperties> supportedLayers = vk::enumerateInstanceLayerProperties();
+        if (debug){
+            std::cout << "device can support the following layers: " << "\n";
+            for (vk::LayerProperties layer : supportedLayers){
+                std::cout << "\t\"" << layer.layerName << "\"\n";
+            }
+        }
+
+        bool layerFound = false;
+        std::vector<const char*> notFoundLayers;
+
+        for (const char* layerName : layers){
+            for (vk::LayerProperties supportedLayer : supportedLayers ){
+                if (strcmp(layerName, supportedLayer.layerName) == 0 ){
+                    notFoundLayers.push_back(layerName);
+                    layerFound = true;
+                }
+            }
+        }
+
+        if (!extensionFound){
+            if (debug) std::cerr << "failed to find required extensions" << std::endl;
+            for (const char* ext : notFoundExtensions){
+                std::cerr << "\t\"" << ext << "\"\n";
+            }
+        }
+
+        if (!layerFound){
+            if (debug) std::cerr << "failed to find required layers" << std::endl;
+            for (const char* layer : layers){
+                std::cerr << "\t\"" << layer << "\"\n";
+            }
+        }
+
+        if (!extensionFound || !layerFound) return false;
+
+        return true;
+    }
+
     vk::Instance make_instance(bool debug, const char* appName){
         if (debug) std::cout << "making an instance" << std::endl;
 
@@ -38,7 +100,17 @@ namespace vkInit{
          */
         if (debug){
             std::cout << "glfw extensions: " << std::endl;
-            for (const char* extName : extensions) std::cout << "\t\"" << extName << "\"\n" << std::endl;
+            for (const char* extName : extensions) std::cout << "\t\"" << extName << "\"\n" ;
+        }
+
+        std::vector<const char*> layers;
+        if (debug) {
+            layers.push_back("VK_LAYER_KHRONOS_validation");
+        }
+
+        if (!supported(extensions, layers, debug)){
+            std::cerr << "failed to find required extensions" << std::endl;
+            return nullptr;
         }
 
         /*
@@ -49,10 +121,8 @@ namespace vkInit{
         vk::InstanceCreateInfo createInfo = vk::InstanceCreateInfo(
                 vk::InstanceCreateFlags(),
                 &appInfo,
-                0,
-                nullptr,
-                static_cast<uint32_t>(extensions.size()),
-                extensions.data());
+                static_cast<uint32_t>(layers.size()), layers.data(),
+                static_cast<uint32_t>(extensions.size()),extensions.data());
 
         try {
             /*
